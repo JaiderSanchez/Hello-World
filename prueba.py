d74dbb893ff58ad6f8084b9deea1e668fbe10a00
JaiderSanchez <jaiderfabiansd@gmail.com>
2024-09-11 17:06:15 -0500

    Creación de 2do archivo Python llamado prueba2.py

---
+++ prueba.py
@@ -0,0 +1,8 @@
+import time
+print("Hello world \n") # Este línea imprime en consola la palabra Hello World
+time.sleep(3)
+print("how are you?")
+print("Second region")
+print(type("Hola")) # La función type me dice que tipo de dato es lo que está en los paréntesis
+print(type(4))
+print(type(5.5))
